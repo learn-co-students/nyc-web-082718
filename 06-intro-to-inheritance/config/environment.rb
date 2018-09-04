3ebe28b3f3cc8d78e204fb3c5bb87150ef23731f
Michael Cheng <michael.cheng@flatironschool.com>
2018-09-04 13:36:24 -0400

    empty inheritance lecture notes

---
+++ 06-intro-to-inheritance/config/environment.rb
@@ -0,0 +1,5 @@
+# Dir[File.join(File.dirname(__FILE__), '../lib', '*.rb')].sort.each { |f| require f }
+
+require 'require_all'
+require 'pry'
+require_all 'lib'
---
+++ 06-intro-to-inheritance/tools/run.rb
@@ -0,0 +1,5 @@
+require_relative '../config/environment.rb'
+
+# Test Code Goes Here
+
+binding.pry
