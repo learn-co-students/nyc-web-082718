16af86ada5cad90e6bdb6873ef2c22e575283ecb
Andrew Cohn <andrewcohn0@gmail.com>
2018-10-02 14:47:40 -0400

    AUTH AUTH AUTH AUTH AUTH AUTH

--- 23-rails-auth/app/controllers/users_controller.rb
+++ 23-rails-auth/app/controllers/users_controller.rb
@@ -1,17 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
 
-    def new
-      @user = User.new
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-      @user = User.create(user_params)
-      redirect_to @user
-    end
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to @user
+  end
+
+  def destroy # DELETE request /users/:id
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = 'You deleted ur account. YEET!'
+    redirect_to new_user_path
+  end
 
 
     private
--- 23-rails-auth/config/routes.rb
+++ 23-rails-auth/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show,:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :new, :create, :destroy]
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
