23704e0edd7c3203073db378639cbd1540add54e
Michael Cheng <michael.cheng@flatironschool.com>
2018-11-27 13:49:16 -0500

    redux shell

--- 46-redux-thunk/frontend/src/index.js
+++ 46-redux-thunk/frontend/src/index.js
@@ -6,7 +6,10 @@ import './teamSelector.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
